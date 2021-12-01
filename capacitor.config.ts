dca194801ba8634c100071560f96a97b7dced3d8
Azhar Asif <47211189+azhar-a@users.noreply.github.com>
2021-12-01 20:32:26 +0300

    server config to run on heroku

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'custom tabs',
+  appName: 'custom-tabs',
   webDir: 'www',
   bundledWebRuntime: false
 };
